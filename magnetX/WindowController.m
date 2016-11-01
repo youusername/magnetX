280a9339d8634ef498ebebc4d30fd5486c66c120
zhangjing <zhangjing@palline.cn>
2016-11-01 09:45:16 +0800

    添加导航栏拖动窗口功能

--- magnetX/WindowController.m
+++ magnetX/WindowController.m
@@ -9,6 +9,8 @@
 #import "WindowController.h"
 #import "AppDelegate.h"
 @interface WindowController ()
+@property BOOL mouseDownInTopToolBar;
+@property (assign) NSPoint initialLocation;
 
 @end
 
@@ -25,5 +27,50 @@
     self.window.styleMask |= NSFullSizeContentViewWindowMask;
     self.window.titlebarAppearsTransparent = YES;
 }
+- (void)mouseDown:(NSEvent *)theEvent
+{
+    self.initialLocation = [theEvent locationInWindow];
+    NSRect windowFrame = [self.window frame];
+    CGFloat heightOfTitleBarAndTopToolBar = 65;
+    NSRect titleFrame = NSMakeRect(0, windowFrame.size.height-heightOfTitleBarAndTopToolBar, windowFrame.size.width, heightOfTitleBarAndTopToolBar);
+    
+    if(NSPointInRect(self.initialLocation, titleFrame))
+    {
+        self.mouseDownInTopToolBar = YES;
+        NSLog(@"Mouse IN");
+    }
+}
+
+-(void)mouseUp:(NSEvent *)theEvent
+{
+    self.mouseDownInTopToolBar = NO;
+    NSLog(@"Mouse UP");
+}
+
+- (void)mouseDragged:(NSEvent *)theEvent
+{
+    if( self.mouseDownInTopToolBar )
+    {
+        NSRect screenVisibleFrame = [[NSScreen mainScreen] visibleFrame];
+        NSRect windowFrame = [self.window frame];
+        
+        NSPoint newOrigin = windowFrame.origin;
+        NSPoint currentLocation = [theEvent locationInWindow];
+        
+        
+        
+        // Update the origin with the difference between the new mouse location and the old mouse location.
+        newOrigin.x += (currentLocation.x - self.initialLocation.x);
+        newOrigin.y += (currentLocation.y - self.initialLocation.y);
+        
+        // Don't let window get dragged up under the menu bar
+        if ((newOrigin.y + windowFrame.size.height) > (screenVisibleFrame.origin.y + screenVisibleFrame.size.height)) {
+            newOrigin.y = screenVisibleFrame.origin.y + (screenVisibleFrame.size.height - windowFrame.size.height);
+        }
+        
+        [self.window setFrameOrigin:newOrigin];
+    }
+}
+
 
 @end
